7cde23a5bb486867b52b290e521b1c1248f31d67
MyoThuKha <myothukha@outlook.sg>
2022-09-24 14:35:12 +0630

    added styles

---
+++ components/navBar.jsx
@@ -0,0 +1,88 @@
+import Link from "next/link";
+const NavBar = () => {
+  return (
+    <nav className="block text-right">
+      <div className="flex items-center justify-between">
+        <h1 className="font-bold uppercase p-3 border-b border-gray-100">
+          <Link href="/">Food Ninja</Link>
+        </h1>
+        <svg
+          className="w-6 h-6 hover:cursor-pointer md:hidden"
+          id="burger-menu"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth="1.5"
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+          />
+        </svg>
+      </div>
+      <ul className="hidden md:block" id="menu-items">
+        <li className="py-1 border-r-4 border-red-400">
+          <a href="#" className="flex items-center justify-end">
+            <span>Home</span>
+            <svg
+              className="w-5 h-6 mx-2"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
+              />
+            </svg>
+          </a>
+        </li>
+        <li className="py-1 border-r-4 border-white">
+          <a href="#" className="flex items-center justify-end">
+            <span>About</span>
+            <svg
+              className="w-5 h-5 mx-2"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z"
+              />
+            </svg>
+          </a>
+        </li>
+        <li className="py-1 border-r-4 border-white">
+          <a href="#" className="flex items-center justify-end">
+            <span>Contact</span>
+            <svg
+              className="w-5 h-5 mx-2"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M21.75 9v.906a2.25 2.25 0 01-1.183 1.981l-6.478 3.488M2.25 9v.906a2.25 2.25 0 001.183 1.981l6.478 3.488m8.839 2.51l-4.66-2.51m0 0l-1.023-.55a2.25 2.25 0 00-2.134 0l-1.022.55m0 0l-4.661 2.51m16.5 1.615a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V8.844a2.25 2.25 0 011.183-1.98l7.5-4.04a2.25 2.25 0 012.134 0l7.5 4.04a2.25 2.25 0 011.183 1.98V19.5z"
+              />
+            </svg>
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./pages/**/*.{js,jsx}", "./components/**/*.{jsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        nunito: ["nunito"],
+      },
+    },
+  },
+  plugins: [],
+};
