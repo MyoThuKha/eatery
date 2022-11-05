a77cafeb46be59f29666f60b359faaffe2d6e219
MyoThuKha <myothukha@outlook.sg>
2022-11-05 18:56:23 +0630

    fixed cls

--- components/choosen.jsx
+++ components/choosen.jsx
@@ -12,19 +12,41 @@ const ChoosenItem = ({ dish }) => {
     return result;
   }, [dish]);
   const displayList =
-    ingredients.length > 10 ? ingredients.slice(0, 10) : ingredients;
+    ingredients.length > 8 ? ingredients.slice(0, 8) : ingredients;
   return (
-    <div>
-      <h1 className="py-2 text-lg">Ingredients</h1>
+    <div className="flex items-center">
+      <svg
+        width="500.4"
+        height="300"
+        viewBox="0 0 737 500"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <rect width="737" height="500" fill="#f2eee9" />
+        <g id="Frame 1">
+          <rect width="737" height="500" fill="#f2eee9" />
+          <g id="Group 1">
+            <circle id="middle" cx="369" cy="250" r="199.5" stroke="black" />
+            <circle id="first" cx="269" cy="250" r="199.5" stroke="black" />
+            <circle id="last" cx="469" cy="250" r="199.5" stroke="black" />
+          </g>
+        </g>
+      </svg>
+
       <div>
-        <div className="customBorder listBox inline-block w-48 overflow-hidden px-5">
-          <h4 className="font-bold">{dish.title}</h4>
-          <ul className="list-disc">
-            {displayList.map((each) => (
-              <li key={ingredients.indexOf(each)}>{each}</li>
-            ))}
-          </ul>
-          <button className="py-4 text-orange-300 underline">See More</button>
+        <h1 className="py-2 text-lg">Ingredients</h1>
+        <div>
+          <div className="customBorder relative listBox inline-block w-48 h-80 overflow-hidden px-5">
+            <h4 className="font-bold">{dish.title}</h4>
+            <ul className="list-disc">
+              {displayList.map((each) => (
+                <li key={ingredients.indexOf(each)}>{each}</li>
+              ))}
+            </ul>
+            <button className="py-4 absolute bottom-2 text-orange-300 underline">
+              See More
+            </button>
+          </div>
         </div>
       </div>
     </div>
