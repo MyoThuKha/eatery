5e401858bc57cb2c15ae1f9cfd097ef192ff9bfa
MyoThuKha <myothukha@outlook.sg>
2022-11-12 21:28:40 +0630

    ...

---
+++ pages/features.jsx
@@ -0,0 +1,11 @@
+const Features = () => {
+  return (
+    <div>
+      <div>
+        <div>Click the instructions to marked as completed.</div>
+      </div>
+    </div>
+  );
+};
+
+export default Features;
